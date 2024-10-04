abae80dc924e8e34829c4ab6589e22854bda210b
enayetsyl <enayetflweb@gmail.com>
2024-10-05 01:03:09 +0600

    like button added

--- src/Components/ProfilePost/PostCard.tsx
+++ src/Components/ProfilePost/PostCard.tsx
@@ -5,6 +5,7 @@ import CardBone from '../Shared/CardBone';
 import CustomInput from '../Shared/CustomInput';
 import CustomButton from '../Shared/CustomButton';
 import Link from 'next/link';
+import LikeButton from '../Shared/LikeButton';
 
 interface PostCardProps {
   userImage: string;
@@ -76,12 +77,16 @@ const PostCard: React.FC<PostCardProps> = ({
         )}
         
         <div className="flex space-x-4 mb-4">
-          <button onClick={handleLike} className="flex items-center text-gray-500 hover:text-blue-500">
+          <LikeButton 
+          onClick={handleLike} 
+          likes={likes}
+          />
+          {/* <button onClick={handleLike} className="flex items-center text-gray-500 hover:text-blue-500">
             <svg className="w-5 h-5 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5" />
             </svg>
             Like ({likes})
-          </button>
+          </button> */}
           <button className="flex items-center text-gray-500 hover:text-blue-500">
             <svg className="w-5 h-5 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" />
